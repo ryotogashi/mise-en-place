fd7c999f521f92b13aaf9e57b5c385b9af3317f6
ryotogashi <ryo.togashi.ca@gmail.com>
2019-12-29 01:14:02 -0800

    add card and picture covered card.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 import Buttons from "./buttons/Buttons";
 import Inputs from "./inputs/Inputs";
+import Cards from "./cards/Cards";
 
 const App = () => (
   <>
     <Buttons />
     <Inputs />
+    <Cards />
   </>
 );
 
