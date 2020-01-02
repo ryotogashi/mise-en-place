61d9ce7a296ba118ae9fef1719c812294f624780
ryotogashi <ryo.togashi.ca@gmail.com>
2020-01-01 23:07:53 -0800

    back

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,4 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components/App";
 
-if (typeof document !== "undefined") {
-  ReactDOM.render(<App />, document.getElementById("root"));
-}
+ReactDOM.render(<App />, document.getElementById("root"));
