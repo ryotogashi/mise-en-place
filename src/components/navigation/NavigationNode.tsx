455629c587b7ed5ba006d27e5e2146f3b4f13827
ryotogashi <ryo.togashi.ca@gmail.com>
2020-01-08 20:12:58 -0800

    change nav bar child node color.

--- src/components/navigation/NavigationNode.tsx
+++ src/components/navigation/NavigationNode.tsx
@@ -13,6 +13,7 @@ const NavigationNode = ({ item, childItems, nodeLevel, display }: Props) => {
 
   const NavigationNodeDiv = styled.div`
     padding: 16px 0 16px ${16 + nodeLevel * 8}px;
+    ${1 < nodeLevel ? "color: #858585;" : ""}
     ${display ? "" : "display: none;"}
   `;
   return (
