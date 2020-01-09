65fadcadd422c1ba26c7f4e4656d3cbf5b31038e
ryotogashi <ryo.togashi.ca@gmail.com>
2020-01-08 19:51:18 -0800

    extract sample navigation data.

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -4,6 +4,7 @@ import Section from "../app-components/Section";
 import IntroPage from "../app-components/IntroPage";
 import SideNavigation from "./SideNavigation";
 import TopNavigation from "./TopNavigation";
+import { navigationItems, navigationItems2 } from "../constants";
 
 const List = () => (
   <Section title="Navigations">
@@ -12,20 +13,12 @@ const List = () => (
         logo={"logo item"}
         navHeight="100%"
         navWidth="50%"
-        items={[
-          ["item 1", null],
-          ["item 2", ["item 2 child 1", "item 2 child 2", "item 2 child 3"]],
-          ["item 3", null],
-          ["item 4", ["item 4 child 1", "item 4 child 2", "item 4 child 3"]]
-        ]}
+        items={navigationItems}
       />
     </IntroPage>
 
     <IntroPage title="Top Navigation" description="Top navigation">
-      <TopNavigation
-        logo={"logo item"}
-        items={["item 1", "item 2", "item 3", "item 4"]}
-      />
+      <TopNavigation logo={"logo item"} items={navigationItems2} />
     </IntroPage>
   </Section>
 );
