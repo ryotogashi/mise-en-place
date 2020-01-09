26b72d476a5cc6a329b534226fda6734e6f0818b
ryotogashi <ryo.togashi.ca@gmail.com>
2020-01-08 19:50:59 -0800

    add navigation node

---
+++ src/components/navigation/NavigationNode.tsx
@@ -0,0 +1,49 @@
+import styled from "@emotion/styled";
+import * as React from "react";
+
+interface Props {
+  item: string;
+  childItems: string[] | null;
+  nodeLevel: number;
+  display: boolean;
+}
+
+const NavigationNode = ({ item, childItems, nodeLevel, display }: Props) => {
+  const [childNodesVisiblity, setChildNodesVisiblity] = React.useState(false);
+
+  const NavigationNodeDiv = styled.div`
+    padding: 16px 0 16px ${16 + nodeLevel * 8}px;
+    ${display ? "" : "display: none;"}
+  `;
+  return (
+    <>
+      <a href={`#${item.toLowerCase()}`}>
+        <NavigationNodeDiv
+          onClick={() => {
+            setChildNodesVisiblity(prev => (prev ? false : true));
+          }}
+        >
+          {item}
+        </NavigationNodeDiv>
+      </a>
+      {childItems
+        ? childItems.map((child, index) => (
+            <NavigationNode
+              key={index}
+              item={child}
+              nodeLevel={nodeLevel + 1}
+              display={childNodesVisiblity}
+            />
+          ))
+        : null}
+    </>
+  );
+};
+
+NavigationNode.defaultProps = {
+  childItems: null,
+  nodeLevel: 1,
+  display: true
+};
+
+export default NavigationNode;
