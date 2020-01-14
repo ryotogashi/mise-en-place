01fa0d17a205ca89a3eb8f8d07caf4831432f9e5
ryotogashi <ryo.togashi.ca@gmail.com>
2020-01-14 14:24:01 -0800

    add width and height props for flat button.

--- src/components/button/FlatButton.tsx
+++ src/components/button/FlatButton.tsx
@@ -1,28 +1,32 @@
 import styled from "@emotion/styled";
 import * as React from "react";
 
-const FlatStyledButton = styled.button`
-  background-color: white;
-  border-radius: 5px;
-  color: gray;
-  height: 30px;
-  width: 150px;
-  cursor: pointer;
-  margin: 16px;
-`;
-
 interface Props {
   value: string;
   onClick: () => {} | null;
+  height: string;
+  width: string;
 }
 
-const FlatButton = ({ value, onClick }: Props) => (
-  <FlatStyledButton onClick={onClick}>{value}</FlatStyledButton>
-);
+const FlatButton = ({ value, onClick, height, width }: Props) => {
+  const FlatStyledButton = styled.button`
+    background-color: white;
+    border-radius: 5px;
+    color: gray;
+    height: ${height};
+    width: ${width};
+    cursor: pointer;
+    margin: 16px;
+  `;
+
+  return <FlatStyledButton onClick={onClick}>{value}</FlatStyledButton>;
+};
 
 FlatButton.defaultProps = {
   value: "button",
-  onClick: null
+  onClick: null,
+  height: "30px",
+  width: "150px"
 };
 
 export default FlatButton;
