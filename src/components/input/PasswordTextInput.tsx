26273ff2aeb9da4aa118d8f38ed28d3ee37a4081
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2020-02-02 12:54:50 -0800

    optionalize password input showhide icon (#26)

--- src/components/input/PasswordTextInput.tsx
+++ src/components/input/PasswordTextInput.tsx
@@ -4,39 +4,12 @@ import * as React from "react";
 import HideIcon from "../../assets/hide.svg";
 import ShowIcon from "../../assets/show.svg";
 
-const PasswordDiv = styled.div`
-  display: inline-block;
-  width: 70.5%;
-  margin: 16px;
-  padding-right: 5px;
-`;
-
-const PasswordLabel = styled.label`
-  display: inline-block;
-  float: right;
-  cursor: pointer;
-`;
-
-const StyledInput = styled.input`
-  height: 24px;
-  width: 99%;
-  margin: 0;
-  padding: 4px;
-  border: 1px solid gray;
-  opacity: 0.4;
-  border-radius: 4px;
-`;
-
-const PasswordImage = styled.img`
-  height: 12px;
-  wieght: 12px;
-`;
-
 interface Props {
   placeholder: string;
+  showHideIcon: boolean;
 }
 
-const PasswordTextInput = ({ placeholder }: Props) => {
+const PasswordTextInput = ({ placeholder, showHideIcon }: Props) => {
   const [imageName, setImageName] = React.useState("show");
   const [inputType, setInputType] = React.useState("password");
 
@@ -45,22 +18,48 @@ const PasswordTextInput = ({ placeholder }: Props) => {
     setInputType(prevValue => (prevValue === "password" ? "text" : "password"));
   };
 
+  const PasswordDiv = styled.div`
+    display: inline-block;
+    width: 70.5%;
+    margin: 16px;
+    padding-right: 5px;
+
+    & > label {
+      display: inline-block;
+      float: right;
+      cursor: pointer;
+    }
+
+    & > input {
+      height: 24px;
+      width: 99%;
+      margin: 0;
+      padding: 4px;
+      border: 1px solid gray;
+      opacity: 0.4;
+      border-radius: 4px;
+    }
+
+    & img {
+      display: ${showHideIcon ? "inline" : "none"};
+      height: 12px;
+      wieght: 12px;
+    }
+  `;
+
   return (
     <PasswordDiv>
-      <PasswordLabel htmlFor="password-input" onClick={handleClick}>
-        <PasswordImage src={imageName === "hide" ? HideIcon : ShowIcon} />
-      </PasswordLabel>
-      <StyledInput
-        name="password-input"
-        type={inputType}
-        placeholder={placeholder}
-      />
+      <label htmlFor="password-input" onClick={handleClick}>
+        <img src={imageName === "hide" ? HideIcon : ShowIcon} />
+      </label>
+      <input name="password-input" type={inputType} placeholder={placeholder} />
     </PasswordDiv>
   );
 };
 
 PasswordTextInput.defaultProps = {
-  placeholder: "password"
+  placeholder: "password",
+  showHideIcon: false
 };
 
 export default PasswordTextInput;
