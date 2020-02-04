b3b56ea8aa4c169dfbe90d36b5ca92935b029460
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-04 12:31:19 -0800

    add  text input with validation

---
+++ src/components/input/TextInputWithValidation.tsx
@@ -0,0 +1,75 @@
+import styled from "@emotion/styled";
+import * as React from "react";
+
+import BasicTextInput from "./BasicTextInput";
+import InputType from "./input-type";
+
+interface Props {
+  validations: [RegExp, string][];
+}
+
+const TextWithValidationMessages = styled.div`
+  & > input {
+    margin-bottom: 4px;
+  }
+
+  .errorColor {
+    border-color: red;
+  }
+
+  .successColor {
+    border-color: blue;
+  }
+
+  & > ul {
+    width: 70%;
+    margin: 0 auto;
+    padding-left: 30px;
+  }
+
+  & > ul > li {
+    list-style: none;
+    text-align: left;
+    font-size: 14px;
+    color: red;
+  }
+`;
+
+const TextInputWithValidation = ({ validations }: Props) => {
+  const [value, setValue] = React.useState("");
+  const [errorMessages, setErrorMessages] = React.useState([] as String[]);
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const val = e.currentTarget.value;
+    setValue(val);
+    console.log(val);
+    setErrorMessages(
+      validations.map(([validation, errorMessage]) => {
+        if (val.match(validation)) return errorMessage;
+      })
+    );
+  };
+  return (
+    <TextWithValidationMessages>
+      <BasicTextInput
+        type={InputType.TEXT}
+        onChange={handleOnChange}
+        value={value}
+        className={
+          value.length === 0
+            ? ""
+            : errorMessages[0] === undefined
+            ? "successColor"
+            : "errorColor"
+        }
+      />
+      <ul>
+        {errorMessages.map((errorMesaage, i) => (
+          <li key={i}>{errorMesaage}</li>
+        ))}
+      </ul>
+    </TextWithValidationMessages>
+  );
+};
+
+export default TextInputWithValidation;
