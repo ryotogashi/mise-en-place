a88f3be42df6e999eb91a8c2b8287e55d602f6b1
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-12 00:27:12 -0800

    fix basic button

--- stories/BasicButton.stories.js
+++ stories/BasicButton.stories.js
@@ -4,8 +4,8 @@ import { action } from "@storybook/addon-actions";
 import BasicButton from "../src/components/button/BasicButton";
 
 export default {
-  title: "Button",
+  title: "Components/Button",
   component: BasicButton
 };
 
-export const _BasicButton = () => <BasicButton value={"basic button"} onClick={action("clicked")} />;
+export const _BasicButton = () => <BasicButton onClick={action("clicked")}>basic button</BasicButton>;
