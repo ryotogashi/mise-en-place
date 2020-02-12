ebf1651d4ca117a553cd1aa935c0b6ee52f33a4d
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-12 00:31:57 -0800

    add story root

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "dist/index.html"));
-});
+app.get("/", (_, res) => res.sendFile(path.resolve(__dirname, "dist/index.html")));
+
+app.get("/story", (_, res) => res.sendFile(path.resolve(__dirname, "storybook-static/index.html")));
 
 app.listen(port);
