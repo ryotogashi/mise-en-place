fc33ee63e6b2f176372253c719e64ddb6dc3ef62
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-12 16:38:32 -0800

    deploy story

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(`${__dirname}/dist`));
+app.use(express.static(`${__dirname}/storybook-static`));
 
 app.get("/", (_, res) => res.sendFile(path.resolve(__dirname, "dist/index.html")));
 
