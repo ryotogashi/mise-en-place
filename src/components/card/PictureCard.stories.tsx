2be46717d539233824bce29563bbd4fee786a457
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2020-02-15 13:46:56 -0800

    Card staories and snapshot tests📚 (#37)
    * add basic card story
    
    * add snapshot test to card♦️

---
+++ src/components/card/PictureCard.stories.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+import PictureCard from "./PictureCard";
+import { cardData } from "../../constants";
+
+export default {
+  title: "Card",
+  component: PictureCard
+};
+
+export const _PictureCard = () => (
+  <PictureCard imgSrc={cardData.cardImg} width="350px" height="230px">
+    <h2>picture card</h2>
+    <p>picture card discrition</p>
+  </PictureCard>
+);
