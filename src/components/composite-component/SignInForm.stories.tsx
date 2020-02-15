6ca32217388a7f2a2a20cf9be94549735c4fe488
Ryo Togashi <35892492+ryotogashi@users.noreply.github.com>
2020-02-15 13:45:54 -0800

    implement searchlist and signinform stories📚 (#36)

---
+++ src/components/composite-component/SignInForm.stories.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+import SignInForm from "./SignInForm";
+
+export default {
+  title: "Composite Component",
+  component: SignInForm
+};
+
+export const _SignInForm = () => <SignInForm />;
