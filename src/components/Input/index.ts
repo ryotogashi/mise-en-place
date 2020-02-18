9f63493362bd1235eb915cf42714ff26c257bab8
ryotogashi <ryo.togashi.ca@gmail.com>
2020-02-18 01:45:52 -0800

    add main export

--- src/components/Input/index.ts
+++ src/components/Input/index.ts
@@ -1,7 +1,8 @@
 export { BasicTextInput } from "./BasicTextInput";
+export { CustomCheckbox } from "./CustomCheckbox";
+export { CustomRadioButton } from "./CustomRadioButton";
 export { CustomRadioButtonGroup } from "./CustomRadioButtonGroup";
 export { DateTextInput } from "./DateTextInput";
 export { PasswordTextInput } from "./PasswordTextInput";
 export { UnderlineTextInput } from "./UnderlineTextInput";
 export { TextInputWithValidation } from "./TextInputWithValidation";
-export { CustomCheckbox } from "./CustomCheckbox";
--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,26 @@
-export {} from "./components/Avatar";
+export { Avatar } from "./components/Avatar";
+export {
+  BasicButton,
+  FlatButton,
+  IconButton,
+  MaterialButton,
+  SquareIconButton,
+  ToggleButton
+} from "./components/Button";
+export { BasicCard, PictureCard } from "./components/Card";
+export { SearchList, SignInForm } from "./components/CompositeComponent";
+export {
+  BasicTextInput,
+  CustomCheckbox,
+  CustomRadioButton,
+  CustomRadioButtonGroup,
+  DateTextInput,
+  PasswordTextInput,
+  TextInputWithValidation,
+  UnderlineTextInput
+} from "./components/Input";
+export { BasicList, CardList } from "./components/List";
+export { SpinLoader } from "./components/Loader";
+export { SideNavigation, TopNavigation } from "./components/Navigation";
+export { BasicSlideshow } from "./components/Slideshow";
+export { primary, secondary, success, danger, warning, info, light, dark, white } from "./constants/colors";
